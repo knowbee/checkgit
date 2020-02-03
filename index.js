6178f5d69c1aed1bd5f707190c46d29b7df39728
Googler <knowbeeinc@gmail.com>
2020-02-03 20:28:20 +0200

    better display

---
+++ index.js
@@ -0,0 +1,56 @@
+const testFolder = "../";
+const { readdirSync } = require("fs");
+
+const term = require("terminal-kit").terminal;
+
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * @param {String} dir
+ * @param {Function} done
+ */
+function checkGit(dir, done) {
+  let results = [];
+
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory() && !file.includes("node_modules")) {
+          readdirSync(file).find(f => {
+            if (f === ".git" && !results.includes(file)) {
+              results.push(file);
+            }
+          });
+          checkGit(file, function(err, res) {
+            if (res === ".git" && !results.includes(file)) {
+              results = results.concat(res);
+            }
+            if (!--pending) done(null, results);
+          });
+        } else {
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+}
+
+checkGit("../", function(err, data) {
+  if (err) {
+    throw err;
+  }
+
+  term.gridMenu(data, function(error, response) {
+    term("\n").eraseLineAfter.green(
+      "#%s selected: %s (%s,%s)\n",
+      response.selectedIndex,
+      response.selectedText
+    );
+    process.exit();
+  });
+});
